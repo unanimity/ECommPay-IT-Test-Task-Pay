1f2132c6311de91a657992543703588a068f72a4
Sergei Kharlamov <unanimity@list.ru>
2019-04-25 08:49:41 +0300

    YII2 - I choose you

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=web',
+    'username' => 'web',
+    'password' => 'web',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
